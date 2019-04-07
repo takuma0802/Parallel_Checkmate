33670662efc06a9e1f0ff18e795c48bc536cf007
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2019-04-07 21:00:01 +0900

    TitleSceneの無駄なコード削除

---
+++ Assets/Scripts/View/TitleScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UniRx;
+
+public class TitleScene : MonoBehaviour
+{
+    [SerializeField] private Button screenButton;
+
+    void Start()
+    {
+        StartBGM();
+        ObserveScreenButton();
+    }
+
+    private void StartBGM()
+    {
+        Sound.LoadBgm("1", "1_title");
+        Sound.LoadSe("5", "5_start");
+        Sound.PlayBgm("1");
+    }
+
+    private void ObserveScreenButton()
+    {
+        screenButton.OnClickAsObservable()
+            .FirstOrDefault()
+            .Subscribe(_ => StartCoroutine(TapScreen()))
+            .AddTo(this);
+    }
+
+
+    private IEnumerator TapScreen()
+    {
+        Sound.PlaySe("5");
+        Sound.StopBgm();
+        Sound.LoadBgm("2", "2_senryak");
+        Sound.PlayBgm("2");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
+
+
