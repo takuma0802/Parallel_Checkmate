27e52ec88d23c1426488273cb94d69148439ca24
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2019-04-14 00:20:11 +0900

    Piece情報のアクセス権限を変更

--- Assets/Scripts/Piece/PieceBase.cs
+++ Assets/Scripts/Piece/PieceBase.cs
@@ -18,38 +18,28 @@ public abstract class PieceBase : IPieceAction
     protected int pieceCost;
     public int PieceCost { get { return pieceCost; } }
 
-    private int row;
     private int column;
-
-    public int Row
-    {
-        get { return row; }
-        set { row = value; }
-    }
-    public int Column
-    {
-        get { return column; }
-        set { column = value; }
-    }
-    
+    private int row;
     private bool isPutted;
     private bool isDestroyed;
-    
-    public bool IsPutted
-    {
-        get { return isPutted; }
-        set { isPutted = value; }
-    }
-    public bool IsDestroyed
-    {
-        get { return isDestroyed; }
-        set { isDestroyed = value; }
-    }
+
+    public int Column { get { return column; } }
+    public int Row { get { return row; } }
+    public bool IsPutted { get { return isPutted; } }
+    public bool IsDestroyed { get { return isDestroyed; } }
 
     public PieceBase(PlayerType player, int pieceNum)
     {
         this.playerType = player;
         this.pieceNum = pieceNum;
+    }
+
+    public void SetPieceInfo(int column, int row, bool isPutted, bool isDestroyed)
+    {   
+        this.column = column;
+        this.row = row;
+        this.isPutted = isPutted;
+        this.isDestroyed = isDestroyed;
     }
 
     public virtual void Move() { }
