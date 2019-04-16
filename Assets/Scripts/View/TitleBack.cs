cbe29f251c06d350a341f25a42fddb007308da27
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2019-04-16 23:44:08 +0900

    Titleへ戻るボタンの編集

---
+++ Assets/Scripts/View/TitleBack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleBack : MonoBehaviour {
+	void Start () {
+		GetComponent<Button>().OnClickAsObservable()
+			.First()
+			.Subscribe(_ => SceneManager.LoadScene("Title",LoadSceneMode.Single));
+	}
+}
